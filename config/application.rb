97abba4d8296911448ffbfa63e72b0381282253f
Brian Ethier <sagacityuf@gmail.com>
2011-04-02 00:05:59 -0800

    rerouted to home controller, removed prototype js, and setup config to generate haml

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Paintfu
   class Application < Rails::Application
+    
+    config.generators do |g|
+      g.template_engine :haml
+    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
