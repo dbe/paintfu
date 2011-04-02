77e1ed18221d10d98c82e3d53543b0596e8bb34f
Brian Ethier <sagacityuf@gmail.com>
2011-04-01 23:52:05 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paintfu::Application.config.secret_token = 'c43394ca8fb5adc853806645d35b343ab389dbaa637a216ebfdbd676ac2545bf99c6c74b818a930c9bf2e35f9ec71c19900a057ddfda3cc98724bfc145157c11'
